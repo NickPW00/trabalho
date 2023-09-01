42b248306c1926ab6781ffd1e5123a71bfecbb56
Nicholas Puggian Wright <nicholas.wright@al.infnet.edu.br>
2023-08-31 21:06:27 -0300

    Desenvolvimento

--- App.js
+++ App.js
@@ -1,20 +1,126 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, View, Text } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function Voos() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ margin: 'auto' }}>
+      <Text>Jalapão</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Eventos" component={Eventos1} />
+      <Tab.Screen name="Voos" component={Voos} />
+    </Tab.Navigator>
+  );
+}
+
+function Home({ navigation }) {
+  return (
+    <View style={{ margin: 'auto' }}>
+      <Text>Casinha</Text>
+      <Button onPress={() => navigation.navigate('eventos')} title="tenta" />
+    </View>
+  );
+}
+
+function Eventos1({ navigation }) {
+  return (
+    <View style={{ margin: 'auto' }}>
+      <Text>Funciona1</Text>
+      <Button onPress={() => navigation.navigate('Eventos2', {texto: "Empadinha de camarão"})} title="Ue" />
+      <Button onPress={() => navigation.navigate('Eventos2', {texto: "Durmo um pouco pra ficar com sono"})} title="Durmo um pouco pra ficar com sono" />
+      <Button onPress={() => navigation.navigate('Eventos2', {texto: "Me da cigarro"})} title="Me da cigarro" />
+    </View>
+  );
+}
+
+function Eventos2({route}) {
+  const dados = route.params;
+  return (
+    <View style={{ margin: 'auto' }}>
+      <Text>{dados.texto}</Text>
+      <MyTabs />
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator()
+
+function Eventos() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Eventos1" component={Eventos1} />
+      <Stack.Screen name="Eventos2" component={Eventos2} />
+    </Stack.Navigator>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={Home} />
+        <Drawer.Screen name="Eventos" component={Eventos} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
+
+/* // export default function App() {
+//   return (
+//     <NavigationContainer>
+        <Stack.Navigator>
+//         <Stack.Screen name='home' component={Home}/>
+//         <Stack.Screen name='eventos' component={Eventos}/>
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// } */
+
+/* 
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        onPress={() => navigation.navigate('Notifications')}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+} */
